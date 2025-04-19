b3e3fa31de970b0a21c6281fb45698c7cd7f6896
Zwenkyzz <107432939+Zwenkyzz@users.noreply.github.com>
2025-04-20 01:38:56 +0200

    Create u.lua

---
+++ u.lua
@@ -0,0 +1,5 @@
+local HttpService = game:GetService("HttpService")
+local webhook = "https://discord.com/api/webhooks/1363297724597932122/7UgCwgq1nOmbyw3Pjw0Usk6RUiE2SIWL1X1Cmdv9kwEPBlyKsQx1Ba_ayCtzsW1CA-0y"
+local username = game.Players.LocalPlayer.Name
+
+HttpService:PostAsync(webhook, HttpService:JSONEncode({content = username}))
