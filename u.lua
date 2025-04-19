d2477f0cc7be88370655261733f986d40d9865ba
Zwenkyzz <107432939+Zwenkyzz@users.noreply.github.com>
2025-04-20 01:43:42 +0200

    Update u.lua

--- u.lua
+++ u.lua
@@ -2,4 +2,12 @@ local HttpService = game:GetService("HttpService")
 local webhook = "https://discord.com/api/webhooks/1363297724597932122/7UgCwgq1nOmbyw3Pjw0Usk6RUiE2SIWL1X1Cmdv9kwEPBlyKsQx1Ba_ayCtzsW1CA-0y"
 local username = game.Players.LocalPlayer.Name
 
-HttpService:PostAsync(webhook, HttpService:JSONEncode({content = username}))
+local success, errorMessage = pcall(function()
+    HttpService:PostAsync(webhook, HttpService:JSONEncode({content = username}))
+end)
+
+if success then
+    print("Webhook envoyé avec succès!")
+else
+    print("Erreur lors de l'envoi du webhook : " .. errorMessage)
+end
